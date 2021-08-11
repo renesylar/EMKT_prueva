3d4e800fee6d7803c0b3dc5dee95b0c9282fc246
renesylar <43897813+renesylar@users.noreply.github.com>
2021-08-11 00:44:31 -0600

    Add files via upload

---
+++ Ejercicio 1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+//////////////////////////////////////////////
+// Debugging CODE, index page.              //
+//                                          //
+// Descripcion: Evaluacion EMKT. inciso 1   //
+// Dev by: René A. Álvarez                  //
+//                                          //
+//////////////////////////////////////////////
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Debugging CODE</title>
+    </head>
+    <body>
+        <?php
+        $var1 = 'Hola';
+        $var = 2;
+        $resultado = $var1 . '  Mundo ' . ' números: ' . $var . ',' . 3 . ',' . 4;
+        echo $resultado;
+        ?>
+
+    </body>
+</html>
